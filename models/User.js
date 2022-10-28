eaee8170d5f8a2fb1fac44e99a46d71494979207
Patrick M <patrickmacdonald07@gmail.com>
2022-10-28 13:57:10 -0400

    set up models

---
+++ models/User.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// define table columns and configuration
+User.init(
+  {
+    // TABLE COLUMN DEFINITIONS GO HERE
+    id: {
+      // use the special Sequelize DataTypes object to provide what type of data it is
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this mean the password must be at least four characters long
+        len: [4],
+      },
+    },
+  },
+  {
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    // pass in our imported sequelize connection (the direct connection to the database)
+    sequelize,
+    timestamps: false,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    freezeTableName: true,
+    // use underscores instead of camel casing
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
+
+module.exports = User;
